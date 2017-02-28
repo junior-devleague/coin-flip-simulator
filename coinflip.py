7eb6666abfe30a3a3c05e10a238209615a00483c
Alan <SpaceToastCoastToCoast@users.noreply.github.com>
2017-02-28 13:07:49 -1000

    Update coinflip.py

--- coinflip.py
+++ coinflip.py
@@ -1,1 +1,16 @@
 import random
+
+def flip(times):
+  flipList = []
+  for i in range(times):
+    result = random.randint(0, 1)
+    if (result == 0):
+      flipList.append("Heads")
+      print("Heads")
+    else:
+      flipList.append("Tails")
+      print("Tails")
+  print(str(flipList.count("Heads")) + str(flipList.count("Tails")))
+
+count = input("How many times will you flip?: ")
+flip(count)
